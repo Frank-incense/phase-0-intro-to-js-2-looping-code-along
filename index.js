1c8b15597cdda19b9c0728db4d7498804f8d90fe
Frankincense <frankincensewelsey@gmail.com>
2025-02-25 11:58:31 +0300

    practice with repetition statements

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const  l = names.length;
+    const thanksMessages = [];
+    for (let i = 0; i < l; i++) {
+        thanksMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanksMessages;
+}
+function countDown(number) {
+    while ( number > -1) {
+        console.log(number);
+        number -= 1;
+    }
+
+}
